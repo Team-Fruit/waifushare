6eae6c53a2da0392bb142197415a175f37946957
sjcl <jrhigasinihonn.0601@gmail.com>
2018-09-12 22:00:58 +0900

    Database connection

--- server/web/src/server.go
+++ server/web/src/server.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+    "log"
+    "os"
     "net/http"
-    
     "github.com/labstack/echo"
+
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/jmoiron/sqlx"
 )
 
 func main() {
+    db, err := sqlx.Connect("mysql", "waifushare:@tcp(db:3306)/waifushare_db")
+    if err != nil {
+        log.Fatalln(err)
+        os.Exit(1)
+    }
+    defer db.Close()
+    
     e := echo.New()
     e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "Hello, World!")
