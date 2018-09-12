afbc4ba349c68961217b54f8d72835dcae76e7af
sjcl <jrhigasinihonn.0601@gmail.com>
2018-09-12 13:21:45 +0900

    Fix syntax error in initialize.sql

--- server/db/initialize.sql
+++ server/db/initialize.sql
@@ -1,11 +1,11 @@
 CREATE USER 'waifushare'@'%';
 GRANT INSERT,SELECT,UPDATE,DELETE ON `waifushare_db`.* TO 'waifushare'@'%';
 
-CREATE DATABASE IF NOT EXITS 'waifushare_db';
+CREATE DATABASE IF NOT EXISTS `waifushare_db`;
 
-CREATE TABLE IF NOT EXITS `waifushare_db`.`user` (
+CREATE TABLE IF NOT EXISTS `waifushare_db`.`user` (
     `id`                INT UNSIGNED    NOT NULL AUTO_INCREMENT,
-    `user_id`           TINYTEXT        NOT NULL,
+    `user_id`           VARCHAR(15)    NOT NULL,
     `display_name`      TINYTEXT        NOT NULL,
     `password_hash`     TINYTEXT        NOT NULL,
     `saved_image_id`    INT UNSIGNED    NOT NULL,
@@ -14,16 +14,16 @@ CREATE TABLE IF NOT EXITS `waifushare_db`.`user` (
     UNIQUE KEY `user_id` ( `user_id` )
 );
 
-CREATE TABLE IF NOT EXITS `waifushare_db`.`twimg` (
+CREATE TABLE IF NOT EXISTS `waifushare_db`.`twimg` (
     `id`            INT UNSIGNED    NOT NULL AUTO_INCREMENT,
-    `twitter_id`    TINYTEXT        NOT NULL,
+    `twitter_id`    VARCHAR(255)        NOT NULL,
     `file_name`     TINYTEXT        NOT NULL,
 
     PRIMARY KEY ( `id` ),
     UNIQUE KEY `twitter_id` ( `twitter_id` )
 );
 
-CREATE TABLE IF NOT EXITS `waifushare_db`.`image` (
+CREATE TABLE IF NOT EXISTS `waifushare_db`.`image` (
     `id`            INT UNSIGNED    NOT NULL AUTO_INCREMENT,
     `created_at`    DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `created_by`    INT UNSIGNED    NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXITS `waifushare_db`.`image` (
         ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXITS `waifushare_db`.`user__image` (
+CREATE TABLE IF NOT EXISTS `waifushare_db`.`user__image` (
     `user_id`   INT UNSIGNED    NOT NULL,
     `image_id`  INT UNSIGNED    NOT NULL,
     `is_like`   INT             NOT NULL,
