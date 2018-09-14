8bcc24efcfcc88cd8bf7cdb45c7b9307fd31c395
sjcl <jrhigasinihonn.0601@gmail.com>
2018-09-14 15:25:13 +0900

    Basic endpoints

--- server/web/src/server.go
+++ server/web/src/server.go
@@ -2,26 +2,102 @@ package main
 
 import (
     "log"
-    "os"
     "net/http"
+    // "starconv" funny typo !
+    "strconv"
+    "database/sql"
+
     "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
 
     _ "github.com/go-sql-driver/mysql"
     "github.com/jmoiron/sqlx"
 )
 
+type (
+    User struct {
+        ID uint
+        UserID string `db:"user_id"`
+        DisplayName sql.NullString `db:"display_name"`
+        PasswordHash sql.NullString `db:"password_hash"`
+        SavedImageId sql.NullInt64 `db:"saved_image_id"`
+        AccountStatus int `db:"account_status"`
+    }
+    Twimg struct {
+        ID uint
+        TwitterID string `db:"twitter_id"`
+        FileName string `db:"file_name"`
+    }
+    Image struct {
+        ID uint
+        CreatedAt string `db:"created_at"`
+        CreatedBy uint `db:"created_by"`
+        TwitterID sql.NullString `db:"twitter_id"`
+    }
+    UserImageLike struct {
+        UserID uint `db:"user_id"`
+        ImageID uint `db:"image_id"`
+        IsLike int `db:"is_like"`
+    }
+)
+
+var (
+    db *sqlx.DB
+)
+
 func main() {
-    db, err := sqlx.Connect("mysql", "waifushare:@tcp(db:3306)/waifushare_db")
+    var err error
+    db, err = sqlx.Connect("mysql", "waifushare:@tcp(db:3306)/waifushare_db")
     if err != nil {
         log.Fatalln(err)
-        os.Exit(1)
     }
     defer db.Close()
     
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
+    
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+
+    e.GET("/", hello)
+    e.POST("/api/v1/image", uploadImage)
+    e.PUT("/api/v1/user/password", updateUserPassword)
+    e.PUT("/api/v1/user", updateUser)
+    e.POST("/api/v1/user", createUser)
+    e.DELETE("/api/v1/user", deleteUser)
+    
     e.Logger.Fatal(e.Start(":8080"))
+}
+
+func hello(c echo.Context) error {
+    stmt, err := db.Preparex("SELECT COUNT(id) FROM user")
+    if err != nil {
+        return err
+    }
+    var cnt int
+    err = stmt.Get(&cnt)
+    if err != nil {
+        return err
+    }
+    return c.String(http.StatusOK, strconv.Itoa(cnt))
+}
+
+func uploadImage(c echo.Context) error {
+    return c.String(http.StatusOK, "upload")
+}
+
+func updateUserPassword(c echo.Context) error {
+    return c.String(http.StatusOK, "userPasswordUpdate")
+}
+
+func updateUser(c echo.Context) error {
+    return c.String(http.StatusOK, "userUpdate")
+}
+
+func createUser(c echo.Context) error {
+    return c.String(http.StatusOK, "createUser")
+}
+
+func deleteUser(c echo.Context) error {
+    return c.String(http.StatusOK, "deleteUser")
 }
 
