3b8db5a1b3300a8bc283769e26c93d0b7e9fff19
sjcl <jrhigasinihonn.0601@gmail.com>
2018-09-16 20:32:52 +0900

    Password hash

--- server/web/src/server.go
+++ server/web/src/server.go
@@ -6,6 +6,7 @@ import (
     // "starconv" funny typo !
     "strconv"
     "database/sql"
+    "golang.org/x/crypto/bcrypt"
 
     "github.com/labstack/echo"
     "github.com/labstack/echo/middleware"
@@ -99,5 +100,15 @@ func createUser(c echo.Context) error {
 
 func deleteUser(c echo.Context) error {
     return c.String(http.StatusOK, "deleteUser")
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
